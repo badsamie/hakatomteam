03544fcd34191c6412bbeade563e49124b3ced17
Asel <nurubekovna@gmail.com>
2023-10-11 19:07:51 +0600

    navbar

--- src/components/ui/Navbar.jsx
+++ src/components/ui/Navbar.jsx
@@ -1,13 +1,70 @@
-import React from "react";
+// src/components/Navbar.js
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import SearchIcon from "@mui/icons-material/Search";
+import PermIdentityIcon from "@mui/icons-material/PermIdentity";
+import ShoppingBagIcon from "@mui/icons-material/ShoppingBag";
+import "./Navbar.css";
 
 const Navbar = () => {
   const navigate = useNavigate();
+  const [scrolled, setScrolled] = useState(false);
+
+  const handleScroll = () => {
+    if (window.scrollY > 20) {
+      setScrolled(true);
+    } else {
+      setScrolled(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <>
-      <button onClick={() => navigate("/")}>Samagan</button>
-      <button onClick={() => navigate("/sabina")}>Sabina</button>
-      <button onClick={() => navigate("/aisuluu")}>Aisuluu</button>
+      <nav className={scrolled ? "navbar scrolled" : "navbar"}>
+        <div className="logo">
+          <img
+            src="https://logohistory.net/wp-content/uploads/2023/07/Ralph-Lauren-Logo.png"
+            alt="Ralph Lauren Logo"
+          />
+        </div>
+        <div className="left-navbar">
+          <ul className="nav-links">
+            <li>
+              <a onClick={() => navigate("/")} href="/">
+                Home
+              </a>
+            </li>
+            <li>
+              <a onClick={() => navigate("/sabina")} href="/products">
+                Products
+              </a>
+            </li>
+            <li>
+              <a onClick={() => navigate("/aisuluu")} href="/about">
+                About
+              </a>
+            </li>
+            <li>
+              <a href="/contact">Contact</a>
+            </li>
+          </ul>
+        </div>
+        <div className="right-navbar">
+          <SearchIcon />
+          <PermIdentityIcon />
+          <FavoriteBorderIcon />
+          <ShoppingBagIcon />
+        </div>
+      </nav>
     </>
   );
 };
