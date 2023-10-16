bd2fabfdd9f78172eea03dc9b36990d4681c0bf0
aisuluu646464 <aisuluualtunbek@gmail.com>
2023-10-16 18:14:02 +0600

    ddbdd

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from "./components/ui/Navbar";
 import MainRoutes from "./routing/MainRoutes";
 import Footer from "./components/ui/Footer";
 
+
 const App = () => {
   return (
     <>
